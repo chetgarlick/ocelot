0afd532dff5dc06eeb26c653e1b29781cce709ad
Chet <chet.garlick@protonmail.com>
2023-12-17 21:12:23 -0600

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    std::cout << "Running." << std::endl;
+    
+
+    return 0;
+};
