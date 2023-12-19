200b570bf390f8af8b8134c1c651bc6c3586d9b8
Chet <chet.garlick@protonmail.com>
2023-12-18 20:40:13 -0600

    split main up a little

--- src/main.cpp
+++ src/main.cpp
@@ -10,41 +10,75 @@ const int HEIGHT = 480;
 
 bool init();
 
-int main(int argc, char* args[]) {
+bool loadMedia();
 
-    printf("Running.");
+void close();
 
-    SDL_Window* window = NULL;
+SDL_Window* window = NULL;
 
-    SDL_Surface* surface = NULL;
+SDL_Surface* surface = NULL;
 
-    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-        printf("Init error: %s\n", SDL_GetError() );
-    } else {
+bool init(){
+    bool success = true;
 
-        window = SDL_CreateWindow("Ocelot", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
-        if(window == NULL){
+    //Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		success = false;
+	} else {
+		//Create window
+		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN );
+		if( window == NULL )
+		{
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        } else {
-            			//Get window surface
+			success = false;
+		}
+		else
+		{
+			//Get window surface
 			surface = SDL_GetWindowSurface( window );
+		}
+	}
 
-			//Fill the surface white
-			SDL_FillRect( surface, NULL, SDL_MapRGB( surface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
+    return success;
+}
 
-			//Wait two seconds
-			SDL_Delay( 2000 );
-        }
-
-    }
-    
-    //Destroy window
-    SDL_DestroyWindow( window );
+void close(){
+    SDL_FreeSurface(surface);
+    SDL_DestroyWindow(window);
 
     SDL_Quit();
+}
+
+int main(int argc, char* args[]) {
+
+    printf("Running.\n");
+
+    if ( !init() ){
+        printf("Initialization failed.\n");
+    } else {
+
+        bool quit = false;
+
+        SDL_Event e;
+
+        while(!quit){
+
+            while( SDL_PollEvent( &e )!= 0 ){
+                if(e.type == SDL_QUIT){
+                    quit = true;
+                }
+            }
+
+			//Apply the image
+			//SDL_BlitSurface( gXOut, NULL, gScreenSurface, NULL );
+			
+			//Update the surface
+			//SDL_UpdateWindowSurface( gWindow );
+        }
+    }
+    close();
 
     return 0;
 };
